671aaa3821ff474aa08cde0b813a01797b7cfed1
dnitra <109307365+dnitra@users.noreply.github.com>
2022-10-21 11:04:57 +0200

    created owner and image models

---
+++ app/Models/Image.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Animal;
+
+class Image extends Model
+{
+    use HasFactory;
+
+
+    public function animal()
+    {
+
+        return $this->belongsTo(Animal::class);
+    }
+}
