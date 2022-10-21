850d0fb555b12a71a6136dac93088d4226769219
Edwin Moore <edwin.moore4888@gmail.com>
2022-10-21 16:08:51 +0200

    styling changes

--- resources/views/owner_detail.blade.php
+++ resources/views/owner_detail.blade.php
@@ -5,12 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Owner Details</title>
-      <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
-{{-- @include('common/messages') --}}
-
-<h1>{{$owner->id ? 'Update owner information:' : 'Add owner information:'}}</h1>
 
 <form action='{{route('owners.update', $owner->id)}}' class='edit_form' method='post'>
 @method('PUT')
@@ -25,19 +22,11 @@
 <input type='number'name='phone' value='{{$owner->phone}}'> <br><br>
 <strong><label>Address:</label></strong> <br> 
 <input type='text'name='address' value='{{$owner->address}}'> <br><br>
-<button type="submit">Update</button> <br> <br>
+<button type="submit">Submit</button>
 
 
 
 </form>
-
-@if ($owner->id)
-    <form action="{{route('owner.delete', $owner->id)}}" method="POST"> 
-        @method('DELETE')
-        @csrf
-        <button type="submit">Delete</button>
-    </form>
-@endif
 
 
 
